e35bd896d60091ca4e2888f3f4e592d52e0bd12d
Julian Le <julian@vinova.sg>
2018-01-16 20:55:42 +0700

    Added create function

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::ProjectsController < ApplicationController
 	def index
 		render json: {status: true}, status: :ok
 	end
+
+	def create
+		p params
+		render json: {status: true}, status: :ok
+	end
 end
