5ea2c929cc84f1883071822cdce85a63263333b0
Julian Le <julian@vinova.sg>
2018-01-16 20:51:50 +0700

    initial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :format_options, command_output: true, log_file: "log/capistrano.log", color
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
